e943f801a3a2fb7fa96ea6547270b5b015929fd1
CohavDev <shaulspam@gmail.com>
2024-08-19 10:45:59 +0300

    fixed client not defined

--- api/sendData.js
+++ api/sendData.js
@@ -2,12 +2,13 @@ import { MongoClient } from "mongodb";
 
 export default async function handler(req, res) {
   const { date, selection, userName } = req.body;
+  var client;
   if (!date || !selection || !userName) {
     return res.status(400).json({ message: "Missing required fields" });
   }
   try {
     console.log("env var = ", process.env.MONGODB_URI);
-    const client = new MongoClient(process.env.MONGODB_URI);
+    client = new MongoClient(process.env.MONGODB_URI);
     await client.connect();
     const db = client.db("store");
     const collection = db.collection("shifts");
