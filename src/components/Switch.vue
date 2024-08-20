5f33ef245d0e43271c0433d65a079565da644ee3
CohavDev <shaulspam@gmail.com>
2024-08-20 15:14:15 +0300

    added users options

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,3 +1,74 @@
-<script setup></script>
-<template></template>
-<style lang="css"></style>
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+const isChecked = ref(false);
+const emit = defineEmits(["change"]);
+function getUserName() {
+  return isChecked.value ? "Shaul" : "Odeliya";
+}
+watch(isChecked, () => {
+  console.log("switch clicked");
+  emit("change", getUserName());
+});
+</script>
+<template>
+  <div>
+    <label class="switch">
+      <input type="checkbox" v-model="isChecked" />
+      <span class="slider"></span>
+    </label>
+    <p>Logged in as {{ isChecked ? "Shaul" : "Odeliya" }}</p>
+  </div>
+</template>
+<style scoped>
+/* The switch - the container */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* Slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #e965d3;
+  transition: 0.4s;
+  border-radius: 34px;
+}
+
+/* Slider before */
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  border-radius: 50%;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  transition: 0.4s;
+}
+
+/* Checked slider */
+input:checked + .slider {
+  background-color: #2196f3;
+}
+
+/* Checked slider before */
+input:checked + .slider:before {
+  transform: translateX(26px);
+}
+</style>
