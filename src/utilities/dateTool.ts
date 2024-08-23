6e452ec9e045247e8d318c11b71024f8aa3c7ef1
CohavDev <shaulspam@gmail.com>
2024-08-23 14:40:49 +0300

    updated dateTool for multiple week support

--- src/utilities/dateTool.ts
+++ src/utilities/dateTool.ts
@@ -1,11 +1,14 @@
 /**
  * Returns an object with the current week's Sunday and Saturday dates in a formatted string.
  *
+ * @param offset - The number of weeks to offset the current date by.
  * @return {{ sundayDateFormatted: string, saturadayFormatted: string }} An object containing the formatted Sunday and Saturday dates.
  */
-export default function getDates() {
+export default function getDates(offset: number) {
+  const specificDate = new Date();
+  specificDate.setDate(specificDate.getDate() + offset * 7);
   const sundayDate = new Date(
-    Date.now() - new Date().getDay() * 24 * 60 * 60 * 1000
+    specificDate.getTime() - new Date().getDay() * 24 * 60 * 60 * 1000
   );
   const saturadayDate = new Date(
     sundayDate.getTime() + 6 * 24 * 60 * 60 * 1000
